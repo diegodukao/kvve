32ee28c9527d7670ac64309726e50277c05d00e0
Diego Dukão <diegodukao@gmail.com>
2017-04-18 22:03:33 -0300

    downloading a video in another thread

--- ytdl/ytdlapp.py
+++ ytdl/ytdlapp.py
@@ -1,3 +1,7 @@
+import threading
+
+import youtube_dl
+
 from kivy.app import App
 
 
@@ -6,7 +10,14 @@ class YtdlApp(App):
 
     Edit ytdl.kv to get started.
     """
+    def download(self, url):
+        options = {}
+        options['ignoreerrors'] = True
+
+        with youtube_dl.YoutubeDL(options) as ydl:
+            url = 'http://www.youtube.com/watch?v=BaW_jenozKcj'
+            dl_thread = threading.Thread(target=ydl.download, args=([url],))
+            dl_thread.start()
 
     def build(self):
         return self.root
-
