b7e0c271646b101d94626c58734f425c3dc50415
Diego Dukão <diegodukao@gmail.com>
2017-05-03 22:44:40 -0300

    changing video source when choosing from the list

--- ytdl/widgets.py
+++ ytdl/widgets.py
@@ -1,6 +1,16 @@
 from pathlib import Path
 
+from kivy.app import App
+from kivy.properties import StringProperty
 from kivymd.list import MDList, TwoLineListItem
+
+
+class VideosListItem(TwoLineListItem):
+    path = StringProperty()
+
+    def on_press(self):
+        app = App.get_running_app()
+        app.root.player.source = self.path
 
 
 class VideosList(MDList):
@@ -13,4 +23,7 @@ class VideosList(MDList):
 
         for fpath in filepaths:
             self.add_widget(
-                TwoLineListItem(text=fpath.name[:40] + "..."))
+                VideosListItem(
+                    text=fpath.name[:40] + "...",
+                    path=str(fpath))
+            )
